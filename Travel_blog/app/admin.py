b58c01f158b9aaccfc75dd424563c47cf71a444d
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-11-20 09:08:48 +0200

    Register model in admin panel

--- Travel_blog/app/admin.py
+++ Travel_blog/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from Travel_blog.app.models import Destination, Comment
+from Travel_blog.app.models import Destination, Comment, Category
 
 
 class CommentInLine(admin.StackedInline):
@@ -9,6 +9,13 @@ class CommentInLine(admin.StackedInline):
 class DestinationAdmin(admin.ModelAdmin):
     list_display = ('title', 'country', 'year')
     inlines = (CommentInLine, )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'created_at')
+    search_fields = ('name', 'description')
+    ordering = ('name',)
 
 
 admin.site.register(Destination, DestinationAdmin)
