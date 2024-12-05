cf937f619b1ccfeb824375919ad5a6d262cc9be6
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-05 12:47:00 +0200

    initial commit

--- Travel_blog/app/views/create.py
+++ Travel_blog/app/views/create.py
@@ -5,24 +5,6 @@ from django.views.generic import CreateView
 from Travel_blog.app.models import Destination
 
 
-# def destination_create(request):
-#     if request.method == 'GET':
-#         context = {
-#             'form': DestinationForm(),
-#         }
-#         return render(request, 'app/destination_create.html', context)
-#     else:
-#         form = DestinationForm(request.POST, request.FILES)
-#         form.instance.user = request.user
-#         if form.is_valid():
-#             form.save()
-#             return redirect('destination list')
-#         context = {
-#             'form': form,
-#         }
-#         return render(request, 'app/destination_create.html', context)
-
-
 class CreateDestinationView(LoginRequiredMixin, CreateView):
     template_name = 'app/destination_create.html'
     model = Destination
