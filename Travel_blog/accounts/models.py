4a70e72b36c2e5744c43462754334d9d5afdfa60
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-05 08:45:46 +0200

    enhance Profile model with image file management

--- Travel_blog/accounts/models.py
+++ Travel_blog/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
+import os
 
 UserModel = get_user_model()
 
@@ -15,3 +16,23 @@ class Profile(models.Model):
         upload_to='profiles',
         blank=True,
     )
+
+    def save(self, *args, **kwargs):
+        # If this is an update and there's a new image
+        if self.pk:
+            try:
+                old_profile = Profile.objects.get(pk=self.pk)
+                if old_profile.profile_image and self.profile_image != old_profile.profile_image:
+                    # Delete the old image file
+                    if os.path.isfile(old_profile.profile_image.path):
+                        os.remove(old_profile.profile_image.path)
+            except Profile.DoesNotExist:
+                pass
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        # Delete the profile image file when the profile is deleted
+        if self.profile_image:
+            if os.path.isfile(self.profile_image.path):
+                os.remove(self.profile_image.path)
+        super().delete(*args, **kwargs)
