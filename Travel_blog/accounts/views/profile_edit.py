f7b200a5ef77fbee30f64ae5fe315947fd4a5aae
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-05 08:54:31 +0200

    enhance edit profile view with login requirement and file management

--- Travel_blog/accounts/views/profile_edit.py
+++ Travel_blog/accounts/views/profile_edit.py
@@ -1,28 +1,34 @@
 from django.shortcuts import render, redirect
-from Travel_blog.accounts.forms.profile_edit import EditProfileForm, EditUserCreationForm
+from django.contrib.auth.decorators import login_required
+from Travel_blog.accounts.forms.profile_edit import EditProfileForm
 from Travel_blog.accounts.models import Profile
+import os
 
 
+@login_required
 def edit_profile(request):
     user = request.user
-    profile = Profile.objects.get(pk=user.id)
-    if request.method == 'GET':
-        context = {
-            'user': user,
-            'user_form': EditUserCreationForm(instance=user),
-            'profile_form': EditProfileForm(instance=profile),
-        }
-        return render(request, 'profile_edit.html', context)
-    else:
-        profile_form = EditProfileForm(request.POST, request.FILES, instance=profile)
-        profile_form.user = request.user
-        if profile_form.is_valid():
-            profile_form.save()
-            return redirect('current user profile')
+    try:
+        profile = Profile.objects.get(user=user)
+    except Profile.DoesNotExist:
+        profile = Profile.objects.create(user=user)
 
-        else:
-            context = {
-                'user': user,
-                'profile_form': profile_form,
-            }
-            return render(request, 'profile_edit.html', context)
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            # If there's a new file being uploaded and an old file exists
+            if 'profile_image' in request.FILES and profile.profile_image:
+                # Delete the old file
+                if os.path.isfile(profile.profile_image.path):
+                    os.remove(profile.profile_image.path)
+            
+            form.save()
+            return redirect('current user profile')
+    else:
+        form = EditProfileForm(instance=profile)
+
+    context = {
+        'user': user,
+        'profile_form': form,
+    }
+    return render(request, 'profile_edit.html', context)
