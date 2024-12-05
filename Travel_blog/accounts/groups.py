e064b94f68a8e7bb10cc34d4e4202f4b504fb86a
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-05 08:41:27 +0200

    Enhance user profile Implement admin group creation with specific permissions

--- Travel_blog/accounts/groups.py
+++ Travel_blog/accounts/groups.py
@@ -3,34 +3,35 @@ from django.contrib.contenttypes.models import ContentType
 from django.db import transaction
 from Travel_blog.app.models import Destination, Comment
 
+
 def create_admin_groups():
     """Create or update admin groups with appropriate permissions"""
-    
+
     # Create SuperAdmin group
     superadmin_group, _ = Group.objects.get_or_create(name='SuperAdmin')
-    
+
     # Create Staff group
     staff_group, _ = Group.objects.get_or_create(name='Staff')
-    
+
     # Get content types
     destination_ct = ContentType.objects.get_for_model(Destination)
     comment_ct = ContentType.objects.get_for_model(Comment)
-    
+
     # Define permissions for SuperAdmin (full CRUD)
     superadmin_permissions = Permission.objects.filter(
         content_type__in=[destination_ct, comment_ct]
     )
-    
+
     # Define permissions for Staff (limited CRUD)
     staff_permissions = Permission.objects.filter(
         content_type__in=[destination_ct, comment_ct],
-        codename__in=['view_destination', 'change_destination', 
-                     'view_comment', 'change_comment', 'delete_comment']
+        codename__in=['view_destination', 'change_destination',
+                      'view_comment', 'change_comment', 'delete_comment']
     )
-    
+
     # Assign permissions to groups
     with transaction.atomic():
         superadmin_group.permissions.set(superadmin_permissions)
         staff_group.permissions.set(staff_permissions)
-        
+
     return superadmin_group, staff_group
