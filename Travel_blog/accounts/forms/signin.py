a92ac33794cc9382102e9b9666311ad7a140c4b5
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-05 08:21:29 +0200

    remove commented-out custom SigninForm definition for cleaner code

--- Travel_blog/accounts/forms/signin.py
+++ Travel_blog/accounts/forms/signin.py
@@ -2,13 +2,6 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 
-# class SigninForm(forms.Form):
-#     username = forms.CharField()
-#     password = forms.CharField(
-#         widget=forms.PasswordInput(),
-#     )
-
-
 class BootstrapFormMixin():
     def __init_fields__(self, *args, **kwargs):
         for (_, field) in self.fields.items():
