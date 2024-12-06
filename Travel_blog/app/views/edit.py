ebcd747bd23bd53bd7902e7f136c21a9f9a8d5f0
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-06 04:59:50 +0200

    initial commit

--- Travel_blog/app/views/edit.py
+++ Travel_blog/app/views/edit.py
@@ -1,15 +1,15 @@
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
-from django.core.exceptions import PermissionDenied
 from Travel_blog.app.forms.edit import EditDestinationForm
 from Travel_blog.app.models import Destination
 import os
 
+
 @login_required
 def destination_edit(request, pk):
     destination = get_object_or_404(Destination, pk=pk)
-    
+
     # SuperAdmin can edit any destination
     if request.user.groups.filter(name='SuperAdmin').exists():
         pass  # Allow full access
@@ -20,7 +20,7 @@ def destination_edit(request, pk):
     elif destination.user != request.user:
         messages.error(request, "You don't have permission to edit this story.")
         return redirect('destination details', pk=pk)
-        
+
     if request.method == 'GET':
         context = {
             'destination': destination,
@@ -30,7 +30,7 @@ def destination_edit(request, pk):
     else:
         old_image_path = destination.image.path if destination.image else None
         form = EditDestinationForm(request.POST, request.FILES, instance=destination)
-        
+
         if form.is_valid():
             # Check if a new image was uploaded
             if 'image' in request.FILES:
@@ -40,7 +40,7 @@ def destination_edit(request, pk):
                         os.remove(old_image_path)
                     except Exception as e:
                         messages.warning(request, f"Could not remove old image: {str(e)}")
-            
+
             form.save()
             messages.success(request, "Story updated successfully!")
             return redirect('destination details', destination.pk)
