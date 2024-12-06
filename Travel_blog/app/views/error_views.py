5420b6c8bceafe1d2c4a274381f5bb60a95756e5
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-06 16:48:49 +0200

    fix 404 page

--- Travel_blog/app/views/error_views.py
+++ Travel_blog/app/views/error_views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponseNotFound
-
 
 def handler404(request, exception=None):
-    template = loader.get_template('404-error.html')
-    context = {
-        'request': request,
-        'exception': exception,
-    }
-    return HttpResponseNotFound(template.render(context, request))
+    return render(request, '404.html', status=404)
