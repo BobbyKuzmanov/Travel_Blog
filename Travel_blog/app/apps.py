5db4eefdb6ae26aeeb4751b3c968866ab569c21d
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-08 07:56:46 +0200

    feat:Add destination image cleanup on user deletion

--- Travel_blog/app/apps.py
+++ Travel_blog/app/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class TravelAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'Travel_blog.app'
+
+    def ready(self):
+        # Import and register signals
+        import Travel_blog.app.signals
---
+++ Travel_blog/app/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from django.apps import apps
+import os
+
+@receiver(pre_delete, sender='auth.User')
+def cleanup_user_destinations(sender, instance, **kwargs):
+    """
+    Signal to clean up destination images when a user is deleted
+    """
+    # Get the Destination model
+    Destination = apps.get_model('app', 'Destination')
+    
+    # Get all destinations for this user
+    destinations = Destination.objects.filter(user=instance)
+    
+    # Delete all destination images
+    for destination in destinations:
+        if destination.image:
+            if os.path.isfile(destination.image.path):
+                try:
+                    os.remove(destination.image.path)
+                except Exception as e:
+                    print(f"Error deleting destination image: {e}")
