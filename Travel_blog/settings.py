980eb1036dcdd2b1bf1c3ab8c7c6e96dd5172185
Bobby <borislav.kuzmanov.bg@gmail.com>
2024-12-09 14:27:14 +0200

    fix:hide allowed hosts

--- Travel_blog/settings.py
+++ Travel_blog/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 # Application definition
 
 INSTALLED_APPS = [
